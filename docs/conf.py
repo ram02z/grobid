6a3bc8ca364dfd2441dc1440e34c10dfc1a92908
Omar Zeghouani <omarzeghouanii@gmail.com>
2022-07-23 17:46:13 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = "GROBID Client"
 author = "Omar Zeghouani"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.1"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
